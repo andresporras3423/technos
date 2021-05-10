84ddfa652e5b7081f2109cfd24ad84514fc6fa46
oscarporras02 <oscarrussi@outlook.com>
2021-05-10 17:16:35 -0500

    update list of words, in word_p.txt file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,17 +7,17 @@ class ApplicationController < ActionController::API
     @@status[false]='false';
     #uncomment below to add the words again
     #if using postgres then replace words.txt with words_p.txt
-    # def initialize
-    #   if Word.all.length==0
-    #     pokemon_file_path = File.join(File.dirname(__FILE__), "../texts/words.txt") 
-    #     file = File.open(pokemon_file_path)
-    #     file_data = file.read
-    #     file_data.each_line do |line|
-    #       ActiveRecord::Base.connection.execute(line)
-    #     end
-    #     file.close
-    #   end
-    # end
+    def initialize
+      if Word.all.length==0
+        pokemon_file_path = File.join(File.dirname(__FILE__), "../texts/words_p.txt") 
+        file = File.open(pokemon_file_path)
+        file_data = file.read
+        file_data.each_line do |line|
+          ActiveRecord::Base.connection.execute(line)
+        end
+        file.close
+      end
+    end
 
 
   def restrict_access
