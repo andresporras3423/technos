19eee7346b9c6124f07e880430080b1bd097025e
oscarporras02 <oscarrussi@outlook.com>
2021-05-10 18:20:16 -0500

    add database.yml to .gitginore

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_04_04_025533) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "technos", force: :cascade do |t|
     t.string "techno_name"
     t.boolean "techno_status"
