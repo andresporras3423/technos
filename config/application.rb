fe039b1f00961c2029c65409bd561942ccf39688
oscarporras02 <oscarrussi@outlook.com>
2021-05-13 09:35:22 -0500

    add delete to allow methods

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module Technos
     Rails.application.config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :patch, :put], credentials: true
+        resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete], credentials: true
       end
     end
 
