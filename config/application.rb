18d02d77b455c05c1f4e22293af36c99c505bc44
oscarporras02 <oscarrussi@outlook.com>
2021-05-13 09:52:14 -0500

    specify origins value in rack cors

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module Technos
     config.middleware.use ActionDispatch::Cookies
     Rails.application.config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*'
+        origins 'http://localhost:3000/'
         resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete]
       end
     end
