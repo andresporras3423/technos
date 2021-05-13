9100b85055a6fdf377ca02fd40f89ea7352e29b8
oscarporras02 <oscarrussi@outlook.com>
2021-05-13 09:44:54 -0500

    remove credentials: true, I don't know what I'm doing

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module Technos
     Rails.application.config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete], credentials: true
+        resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete]
       end
     end
 
