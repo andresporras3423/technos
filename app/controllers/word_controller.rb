b56750c02c2c0fdab1efc687e7b745d7c12cdf42
oscarporras02 <oscarrussi@outlook.com>
2021-05-14 17:01:49 -0500

    using post instead of get as method type for search cause too many params to use i nthe url

--- app/controllers/word_controller.rb
+++ app/controllers/word_controller.rb
@@ -26,7 +26,7 @@ class WordController < ApplicationController
     end
 
     def search
-      filter = @@status[params[:sort_by_word]] ? 'word' : 'created_at'
+      filter = params[:sort_by_word] ? 'word' : 'created_at'
       user_words=[]
       if params[:search]==1
         user_words=Word.where("user_id=#{@user.id}
