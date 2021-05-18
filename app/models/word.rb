b03c39d09bdedab422115b31b90740adbfde1fa7
oscarporras02 <oscarrussi@outlook.com>
2021-05-17 19:13:08 -0500

    add combined uniques and presence restriction on words and technos models

--- app/models/word.rb
+++ app/models/word.rb
@@ -6,7 +6,7 @@ class Word < ApplicationRecord
     validate :uniq_user_word_techno
 
     def uniq_user_word_techno
-        if Word.where(user_id: self.user_id, word: self.word, techno_id: self.techno_id).limit(1).length==0
+        if Word.where(user_id: self.user_id, word: self.word, techno_id: self.techno_id).limit(1).length==1
             errors.add("unique_word_user_techno", "the combination of user_id, word and techno_id must be unique")
         end 
     end
