bad718f18259f665f254aaba59e1674ca7469723
oscarporras02 <oscarrussi@outlook.com>
2021-05-17 19:34:31 -0500

    fix error in uniq validation

--- app/models/word.rb
+++ app/models/word.rb
@@ -7,7 +7,7 @@ class Word < ApplicationRecord
 
     def uniq_user_word_techno
         if Word.where(user_id: user_id, word: word, techno_id: techno_id).limit(1).length==1
-            errors.add(:word:, "the combination of user_id, word and techno_id must be unique")
+            errors.add(:word, "the combination of user_id, word and techno_id must be unique")
         end 
     end
 end
