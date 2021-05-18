2e424d5be3d4c0c2b976fed99a2620addb182e02
oscarporras02 <oscarrussi@outlook.com>
2021-05-17 19:56:00 -0500

    fix error typo in word model

--- app/models/word.rb
+++ app/models/word.rb
@@ -3,11 +3,11 @@ class Word < ApplicationRecord
     belongs_to :user
     validates :word, presence: true, length: { minimum: 1 }
     validates :translation, presence: true, length: { minimum: 1 }
-    validates :uniq_user_word_techno
+    validate :uniq_user_word_techno
 
     def uniq_user_word_techno
-        if Word.where(user_id: user_id, word: word, techno_id: techno_id).limit(1).length==1
-            errors.add(:word, "the combination of user_id, word and techno_id must be unique")
+        if Word.where(user_id: self.user_id, word: self.word, techno_id: self.techno_id).limit(1).length==1
+            self.errors.add(:word, "the combination of user_id, word and techno_id must be unique")
         end 
     end
 end
