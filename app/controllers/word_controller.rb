b8e85a99b79e34c8d46a52fe62356858c477bbdb
oscarporras02 <oscarrussi@outlook.com>
2021-05-18 18:44:23 -0500

    update validation to work when update word

--- app/controllers/word_controller.rb
+++ app/controllers/word_controller.rb
@@ -28,25 +28,25 @@ class WordController < ApplicationController
     def search
       filter = params[:sort_by_word] ? 'word' : 'created_at'
       user_words=[]
-      if params[:search]==1
+      if params[:search]==1 # contains
         user_words=Word.where("user_id=#{@user.id}
           and ('#{params[:word]}'='' or word LIKE '%#{params[:word]}%')
           and ('#{params[:translation]}'='' or translation LIKE '%#{params[:translation]}%')
           and (#{params[:techno_id]}=-1 or techno_id=#{params[:techno_id]})
           ").order(filter)
-      elsif params[:search]==2
+      elsif params[:search]==2 # starts
         user_words=Word.where("user_id=#{@user.id}
             and ('#{params[:word]}'='' or word LIKE '#{params[:word]}%')
             and ('#{params[:translation]}'='' or translation LIKE '#{params[:translation]}%')
             and (#{params[:techno_id]}=-1 or techno_id=#{params[:techno_id]})
         ").order(filter)
-      elsif params[:search]==3
+      elsif params[:search]==3 # ends
         user_words=Word.where("user_id=#{@user.id}
             and ('#{params[:word]}'='' or word LIKE '%#{params[:word]}')
             and ('#{params[:translation]}'='' or translation LIKE '%#{params[:translation]}')
             and (#{params[:techno_id]}=-1 or techno_id=#{params[:techno_id]})
         ").order(filter)
-      elsif  params[:search]==4
+      elsif  params[:search]==4 # equals
         user_words=Word.where("user_id=#{@user.id}
           and ('#{params[:word]}'='' or word = '#{params[:word]}')
           and ('#{params[:translation]}'='' or translation = '#{params[:translation]}')
