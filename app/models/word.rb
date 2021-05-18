5a7745627ded95fc3b605ce18e951189abfdefa6
oscarporras02 <oscarrussi@outlook.com>
2021-05-17 19:12:48 -0500

    add error for unique combination in models validations

--- app/models/word.rb
+++ app/models/word.rb
@@ -3,4 +3,11 @@ class Word < ApplicationRecord
     belongs_to :user
     validates :word, presence: true, length: { minimum: 1 }
     validates :translation, presence: true, length: { minimum: 1 }
+    validate :uniq_user_word_techno
+
+    def uniq_user_word_techno
+        if Word.where(user_id: self.user_id, word: self.word, techno_id: self.techno_id).limit(1).length==0
+            errors.add("unique_word_user_techno", "the combination of user_id, word and techno_id must be unique")
+        end 
+    end
 end
