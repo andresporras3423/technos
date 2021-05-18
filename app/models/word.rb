36c8647b0e050f508631092382416529ea33ebb6
oscarporras02 <oscarrussi@outlook.com>
2021-05-17 19:49:58 -0500

    fix error typo in word model

--- app/models/word.rb
+++ app/models/word.rb
@@ -3,7 +3,7 @@ class Word < ApplicationRecord
     belongs_to :user
     validates :word, presence: true, length: { minimum: 1 }
     validates :translation, presence: true, length: { minimum: 1 }
-    validate :uniq_user_word_techno
+    validates :uniq_user_word_techno
 
     def uniq_user_word_techno
         if Word.where(user_id: user_id, word: word, techno_id: techno_id).limit(1).length==1
